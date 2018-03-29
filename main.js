9ce1ecfb8364a1ea65c2d4a48f787ccb5c00708c
Aaron Guevara <AGuevara@clouddev.epiqcorp.com>
2018-03-29 08:38:25 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'img/vmSocks-green-onWhite.jpg',
+        url: 'http://www.google.com',
+        onSale: false,
+        details: ["80% cotton","20% polyester","gender-neutral"]
+    }
+});
